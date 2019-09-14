970ff7a9cc5ae86ad71d8cba43e33d78dd4fc461
Lgmrszd <m.v.bobrov@yandex.ru>
2019-09-15 00:15:36 +0300

    Some content

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 # from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
+
+
+@app.route('/cool_video')
+def cool_video():
+    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 
 if __name__ == '__main__':
