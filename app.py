ba1ab987f7883da6147cae13aa29f86ce0484e1d
Lgmrszd <m.v.bobrov@yandex.ru>
2019-09-14 22:27:03 +0300

    Working example

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+# from flask_socketio import SocketIO
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
